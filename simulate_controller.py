eb2df9ef72ec7a0d13051bf035d84dbee644985a
MOI <jmartinsaquet@lrtechnologies.fr>
2025-04-01 11:02:26 +0200

    sim correction sims to work without triggering unecessary callbacks

--- simulate_controller.py
+++ simulate_controller.py
@@ -14,17 +14,27 @@ This script is a script that is going to load the trained model
 and be tested on the user directly to see if there is any differences between with and without the controller
 '''
 
+DEBUG_ONMOVE_POSITION = True
+DEBUG_CORRECTION_STEP = True
+
 class Corrector(object):
     """Controller with the model"""
 
     def __init__(self, model, device):
 
         self.corrector = model.eval()
+        self.device = device
+
+        # initialization
         self.x_poses = []
         self.y_poses = []
-
-
-        self.device = device
+        self.do_correction = True # we can modify the mouse position 
+        self.dxs = [0.]
+        self.dys = [0.]
+        self.t = time.time()
+        self.k = 0
+        self.correct = True
+        
         # get the mouse
         self.mouse = mouse.Controller()
         self.x_poses.append(self.mouse.position[0])
@@ -33,103 +43,118 @@ class Corrector(object):
         self.min_input = np.array([0, 0, -80, -80, 0])
         self.max_input = np.array([1920, 1080, 80, 80, 500])
 
-        self.flag = True # we can modify the mouse position 
 
-        self.dxs = [0.]
-        self.dys = [0.]
 
-        self.t = time.time()
-        self.k = 0
 
-        self.correct = False
         print("Controller ready to be used !")
         print("Correction activated ? ", self.correct)
 
+    def correct_displacement(self, x, y):
+        # x and y are the cursor position after displacement
+
+        # delta t wiht last dispalcement
+        dt = time.time() - self.t 
+        
+        # get env_input
+        dx = x - self.x_poses[-1]
+        dy = y - self.y_poses[-1]
+
+        # keep dx and dy for log purpose
+        # self.dxs.append(dx)
+        # self.dys.append(dy)
+
+        model_input = np.array([self.x_poses[-1], self.x_poses[-1], dx, dy, dt]) # should i take current x and y or last one ? 
+
+        # scale input
+        # model_input = self.scale_input(model_input)
+        
+        model_input = torch.from_numpy(model_input).float()
+
+
+        # get the prediction
+        with torch.inference_mode():
+            prediction = self.corrector(model_input)
+    
+        
+        dx = prediction[0]
+        dy = prediction[1]
+        
+        # compute the predicted cursor position
+        cursor_position = (self.x_poses[-1] + dx, self.y_poses[-1] + dy)
+        
+        print("x, y : ", (x,y), "cursor_position : ", cursor_position)
+        
+        # move the cursor position to the predicted position
+        self.mouse.position = cursor_position
+
+        # update of time
+        self.t = time.time()
+
+        self.x_poses.append(self.mouse.position[0])
+        self.y_poses.append(self.mouse.position[1])
+        
+        # return correction has been done 
+        return True
+    
+
+    def set_listener(self, listener):
+
+        self.listener = listener
 
     def main(self):
 
         self.i = 0
         # do it still forever
         while True:
-            print("self.i ", self.i)
-            if self.i == 1000:
-                print("move ! ")
-                mouse.position = (0, 0)
-            self.i += 1
+            pass
+            # print("self.i ", self.i)
+            # if self.i == 1000:
+            #     print("move ! ")
+            #     mouse.position = (0, 0)
+            # self.i += 1
             # self.mouse.move(10,20)
 
                 # self.flag = False
 
-    
+    def scale_model_input(self, x):
+
+        return (x - self.min_input) / (self.max_input - self.min_input) * 2 - 1 
+
     def on_move(self, x, y):
+        
+        has_corrected = False
+
+        if DEBUG_ONMOVE_POSITION:
+            print("x : ", x, " y : ", y, "displacement_number : ", self.k)
+            print("correct ? : ", self.correct, "do correction ?", self.do_correction)
+
+        if self.do_correction and self.correct:
+            has_corrected = self.correct_displacement(x, y)
+            # if it has corrected the movement then we need to deactivate the correction for one turn (mouse.position = (0, 0) trigger callback)
+            # print("we are after the correction step (i think we don't reach that step ! )")
+            if has_corrected:
+                self.do_correction = False
+
+        self.k += 1
+
+        # redo the correction if the flag is down
+        if not self.do_correction and not has_corrected:
+            self.do_correction = True
 
 
-        # self.flag = True
-        # print("x : ", x, " y : ", y, " i : ", self.k)
-        # self.k += 1
-        # # get the prediction
-        if self.flag:
-            self.flag = not self.flag
-            
-            t_start = time.time()
-
-            
-            
-            dx = x - self.x_poses[-1]
-            dy = y - self.y_poses[-1]
-            # # mag = np.sqrt(dx**2 + dy**2)
-
-            dt = time.time() - self.t
-
-            self.dxs.append(dx)
-            self.dys.append(dy)
-
-            model_input = np.array([[self.x_poses[-1], self.y_poses[-1], dx, dy, dt]])
-            model_input = (model_input - self.min_input) / (self.max_input - self.min_input) * 2 - 1 
-
-            model_input = torch.from_numpy(model_input).float()
-            # model_input = torch.tensor(model_input, dtype=torch.float)
-
-            # # get the prediction
-            with torch.inference_mode():
-                prediction = self.corrector.forward(model_input)
 
 
-            new_cursor_position = np.round((self.x_poses[-1], self.y_poses[-1]) + prediction.cpu().detach().numpy())
-            self.mouse.position = (new_cursor_position[0][0], new_cursor_position[0][1])
-
-            self.x_poses.append(x)
-            self.y_poses.append(y)
-            
-            # # print("flag : , i :", self.flag, self.k) 
-            # if self.correct and self.flag and dx == 0 and dy == 0: 
-            #     self.mouse.move(np.round(new_cursor_position[0]), np.round(new_cursor_position[1])) # this is triggering a new event...
-            #     print(" correction off !" )
-            #     self.flag = not self.flag
-            #     self.k = 0
-            #     # time.sleep(0.2)
-            # elif not self.flag and self.k >10:
-            #     print(" correction on !" )
-            #     self.flag = not self.flag
-            # # time.sleep(0.2)
-            # # self.mouse.position = new_cursor_position
-
-            self.k += 1
-
-            # print("dx : ", dx, " dy : ", dy, "dt : ", dt)
-            # print("input model {}, pred : {}".format(model_input, prediction))
-            print("real cursor {}, pred cursor {}".format((x, y), new_cursor_position))
-
-            print(f"onmove time elapsed : {time.time() - t_start}")
-            self.flag = not self.flag
 
 
     def on_press(self, key):
 
         if key == keyboard.Key.esc:
-            exit()
+            self.listener.stop()
+            # exit()
         if key == keyboard.Key.space:
-            self.mouse.position = (0, 0)
+            self.mouse.position = (1, 0)
+        if key == keyboard.Key.ctrl:
+            self.correct = not self.correct
 
 
 
@@ -150,5 +175,8 @@ if __name__ == "__main__":
     # controller creation 
     corrector = Corrector(model, device)
     with mouse.Listener(on_move=corrector.on_move), keyboard.Listener(on_press=corrector.on_press) as listener:
+        corrector.set_listener(listener)
         listener.join()
+        print("----------- Control for experiment : {} with model : {} finished -----------".format(experiment, model_type))
+        listener.stop()
 
